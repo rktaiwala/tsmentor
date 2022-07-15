a66b0849d7d923c2ef4a5015a9a48e0b4bc27724
Rahul Taiwala <rktaiwala@gmail.com>
2022-07-15 21:48:34 +0530

    added new module tabs

--- ts-mentor-addon/vendor/composer/autoload_static.php
+++ ts-mentor-addon/vendor/composer/autoload_static.php
@@ -13,10 +13,15 @@ class ComposerStaticInit73dba83455376f3e87cfb01282cb6ebd
         'TS\\Classes\\Dashboard' => __DIR__ . '/../..' . '/includes/classes/dashboard.php',
         'TS\\Classes\\ModuleManager' => __DIR__ . '/../..' . '/includes/classes/module-manager.php',
         'TS\\Helper' => __DIR__ . '/../..' . '/includes/helper.php',
+        'TS\\Modules\\Advancetabs\\Module' => __DIR__ . '/../..' . '/includes/modules/advancetabs/module.php',
         'TS\\Modules\\Woocommerce\\Module' => __DIR__ . '/../..' . '/includes/modules/woocommerce/module.php',
+        'TS\\Modules\\Woocommerce\\Widgets\\TsAdvTabs' => __DIR__ . '/../..' . '/includes/modules/advancetabs/widgets/ts-adv-tabs.php',
         'TS\\Modules\\Woocommerce\\Widgets\\TsWooRpByCat' => __DIR__ . '/../..' . '/includes/modules/woocommerce/widgets/rp.php',
         'TS\\Modules\\Woocommerce\\Widgets\\TsWooTitle' => __DIR__ . '/../..' . '/includes/modules/woocommerce/widgets/ts-woo-title.php',
         'TS\\Plugin' => __DIR__ . '/../..' . '/includes/bootstrap.php',
+        'TS\\Traits\\Core' => __DIR__ . '/../..' . '/includes/traits/core.php',
+        'TS\\Traits\\Enqueue' => __DIR__ . '/../..' . '/includes/traits/enqueue.php',
+        'TS\\Traits\\Generator' => __DIR__ . '/../..' . '/includes/traits/generator.php',
         'TS\\Ts' => __DIR__ . '/../..' . '/includes/ts.php',
     );
 
