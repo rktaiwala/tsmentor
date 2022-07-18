5a80f27e84f9d21894f8a7f0cfb9c51746b08e16
Rahul Taiwala <rktaiwala@gmail.com>
2022-07-18 17:51:25 +0530

    added dashboard home tab files

--- ts-mentor-addon/includes/templates/admin/dashboard-main.php
+++ ts-mentor-addon/includes/templates/admin/dashboard-main.php
@@ -6,23 +6,24 @@
 defined( 'ABSPATH' ) || die();
 ?>
 <div class="ts">
-<div class="container">
-    <h1 class="screen-reader-text"><?php esc_html_e( 'Techsarathy Elementor Addons', 'happy-elementor-addons' ); ?></h1>
-    <form class="ts-dashboard" id="ha-dashboard-form">
-        <div class="ha-dashboard-tabs" role="tablist">
-            <div class="ha-dashboard-tabs__nav">
+<div class="container m-3">
+    
+    <form class="ts-dashboard" id="ts-dashboard-form">
+        <div class="ts-dashboard-tabs" role="tablist">
+            <div class="tabs">
+                <ul>
                 <?php
                 $tab_count = 1;
                 foreach ( self::get_tabs() as $slug => $data ) :
                     $slug = esc_attr( strtolower( $slug ) );
-                    $class = 'ha-dashboard-tabs__nav-item ha-dashboard-tabs__nav-item--' . $slug;
+                    $class = 'ts-tabs__nav-item--' . $slug;
 
                     if ( empty( $data['renderer'] ) || ! is_callable( $data['renderer'] ) ) {
                         $class .= ' nav-item-is--link';
                     }
 
                     if ( $tab_count === 1 ) {
-                        $class .= ' tab--is-active';
+                        $class .= ' is-active';
                     }
 
                     if ( ! empty( $data['href'] ) ) {
@@ -31,18 +32,19 @@ defined( 'ABSPATH' ) || die();
                         $href = '#' . $slug;
                     }
 
-                    printf( '<a href="%1$s" aria-controls="tab-content-%2$s" id="tab-nav-%2$s" class="%3$s" role="tab">%4$s</a>',
+                    printf( '<li class="%3$s"><a href="%1$s" aria-controls="tab-content-%2$s" id="tab-nav-%2$s" class="" role="tab">%4$s</a></li>',
                         $href,
                         $slug,
                         $class,
-                        isset( $data['title'] ) ? $data['title'] : sprintf( esc_html__( 'Tab %s', 'happy-elementor-addons' ), $tab_count )
+                        isset( $data['title'] ) ? $data['title'] : sprintf( esc_html__( 'Tab %s', 'tsmentor' ), $tab_count )
                     );
 
                     ++$tab_count;
                 endforeach;
                 ?>
 
-                <button disabled class="ha-dashboard-tabs__nav-btn ha-dashboard-btn ha-dashboard-btn--lg ha-dashboard-btn--save" type="submit"><?php esc_html_e( 'Save Settings', 'happy-elementor-addons' ); ?></button>
+                <li><button disabled class="ha-dashboard-tabs__nav-btn ha-dashboard-btn ha-dashboard-btn--lg ha-dashboard-btn--save" type="submit"><?php esc_html_e( 'Save Settings', 'happy-elementor-addons' ); ?></button></li>
+                </ul>
             </div>
             <div class="ha-dashboard-tabs__content">
                 <?php
