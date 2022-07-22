634d09ba0933a778172061f975ded8dfe5916770
Rahul Taiwala <rktaiwala@gmail.com>
2022-07-22 17:06:27 +0530

    added new widget Custom Post List

--- ts-mentor-addon/includes/traits/ajax.php
+++ ts-mentor-addon/includes/traits/ajax.php
@@ -16,6 +16,8 @@ trait Ajax {
         
         add_action( 'wp_ajax_ts_select2_search_post', [ $this, 'select2_ajax_posts_filter_autocomplete' ] );
 		add_action( 'wp_ajax_nopriv_ts_select2_search_post', [ $this, 'select2_ajax_posts_filter_autocomplete' ] );
+        add_action( 'wp_ajax_ts_select2_get_title', [ $this, 'select2_ajax_get_posts_value_titles' ] );
+		add_action( 'wp_ajax_nopriv_ts_select2_get_title', [ $this, 'select2_ajax_get_posts_value_titles' ] );
     }
     
     /**
@@ -73,5 +75,58 @@ trait Ajax {
 			}
 		}
 		wp_send_json( [ 'results' => $results ] );
+	}
+    
+    public function select2_ajax_get_posts_value_titles() {
+
+		if ( empty( $_POST['id'] ) ) {
+			wp_send_json_error( [] );
+		}
+
+		if ( empty( array_filter( $_POST['id'] ) ) ) {
+			wp_send_json_error( [] );
+		}
+		$ids         = array_map( 'intval', $_POST['id'] );
+		$source_name = ! empty( $_POST['source_name'] ) ? sanitize_text_field( $_POST['source_name'] ) : '';
+
+		switch ( $source_name ) {
+			case 'taxonomy':
+				$args = [
+					'hide_empty' => false,
+					'orderby'    => 'name',
+					'order'      => 'ASC',
+					'include'    => implode( ',', $ids ),
+				];
+
+				if ( $_POST['post_type'] !== 'all' ) {
+					$args['taxonomy'] = sanitize_text_field( $_POST['post_type'] );
+				}
+
+				$response = wp_list_pluck( get_terms( $args ), 'name', 'term_id' );
+				break;
+			case 'user':
+				$users = [];
+
+				foreach ( get_users( [ 'include' => $ids ] ) as $user ) {
+					$user_id           = $user->ID;
+					$user_name         = $user->display_name;
+					$users[ $user_id ] = $user_name;
+				}
+
+				$response = $users;
+				break;
+			default:
+				$post_info = get_posts( [
+					'post_type' => sanitize_text_field( $_POST['post_type'] ),
+					'include'   => implode( ',', $ids )
+				] );
+				$response  = wp_list_pluck( $post_info, 'post_title', 'ID' );
+		}
+
+		if ( ! empty( $response ) ) {
+			wp_send_json_success( [ 'results' => $response ] );
+		} else {
+			wp_send_json_error( [] );
+		}
 	}
 }
